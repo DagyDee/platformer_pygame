7496c98210e3dd307331b421c21101f297059bc4
Dagmar Vodáková <d.vodakova@seznam.cz>
2025-08-21 15:42:29 +0200

    Initial commit: add project structure, basic game loop and window
    - Set up project folder and files
    - Implement minimal game loop
    - Create game window

---
+++ engine/game.py
@@ -0,0 +1,21 @@
+import pygame
+
+class Game:
+    def __init__(self):
+        self._is_running = True
+        pygame.init()
+        self.display: pygame.Surface = pygame.display.set_mode((600, 600))
+    
+    def end(self):
+        self._is_running = False
+
+    def run(self) -> None:
+        while self._is_running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.end()
+            pygame.display.flip()
+        pygame.quit()
+
+game = Game()
+game.run()
